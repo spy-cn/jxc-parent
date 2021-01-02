6fb607b64f16bb893a0196404bfaf35bc599bf4a
spy <zl_zhaolei01@163.com>
2021-01-02 10:14:33 +0800

    初始化空项目

---
+++ src/main/java/com/spy/jxc/JxcParentApplication.java
@@ -0,0 +1,13 @@
+package com.spy.jxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxcParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxcParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spy/jxc/JxcParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spy.jxc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxcParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
